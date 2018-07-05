e10843cac69cbf856b14ffa1522aa95494862c1d
Michael Carlos <michaelpcarlos@gmail.com>
2018-07-05 20:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const currentLine = (line) => {
   let arr = [];
   
   if (line.length === 0) {
-    return 'The line is currently empty';
+    return 'The line is currently empty.';
   }
   
   for (var i = 0; i < line.length; i++) {
