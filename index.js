958c77fcca7b4457e30c70df0466c531557eda7b
Michael Carlos <michaelpcarlos@gmail.com>
2018-07-05 20:41:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
-function takeANumber(line, name) {
+const takeANumber = (line, name) => {
   line.push(name);
   
   return `Welcome, ${name}. You are number ${line.length} in line.`;
-}
-
-function nowServing(line) {
-  if (line.length === 0) {
-    return 'There is nobody waiting to be served!';
-  } else {
-    return `Currently serving ${line.shift()}.`;
-  }
-}
-
-function currentLine(line) {
-  var arr = [];
-  
-  if (line.length === 0) {
-    return 'The line is currently empty.';
-  }
-  
-  for (var i = 0; i < line.length; i++) {
-    var names = line[i];
-    
-    arr.push(`${i + 1}. ${names}`);
-  }
-  
-  return `The line is currently: ${arr.join(', ')}`;
-}
+};
