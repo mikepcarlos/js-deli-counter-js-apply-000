0c5b650f70a124a5a7cfa7f32cfc22644e2c8f6c
Michael Carlos <michaelpcarlos@gmail.com>
2018-07-05 20:54:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const nowServing = (line) => {
   return (line.length === 0) ? 'There is nobody waiting to be served!' : `Currently serving ${line.shift()}.`;
 };
 
+const currentLine = (line) => {
+  
+  let arr = [];
+  
+  if (line.length === 0) {
+    return 'The line is currently empty';
+  }
+  
+  for (var i = 0; i < line.length; i++) {
+    var names = line[i];
+    arr.push(`${i + 1}. ${names}`);
+  }
+  
+  return `The line is currently: ${arr.join(', ')}`;
+};
