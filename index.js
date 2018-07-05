bab0cf889d93eda7abe69a18781e6597fb0d47e7
Michael Carlos <michaelpcarlos@gmail.com>
2018-07-05 20:45:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const takeANumber = (line, name) => {
 };
 
 const nowServing = (line) => {
-  return (line.length === 0) ? 'There is nobody waiting to be served' : `Currently serving ${line.shift()}.`;
+  return (line.length === 0) ? 'There is nobody waiting to be served!' : `Currently serving ${line.shift()}.`;
 };
